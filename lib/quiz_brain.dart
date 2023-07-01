7950d2eb8388ccfc39f3dfff54fb9de41abffe23
AndresLince <lynxdarmatron@gmail.com>
2023-06-30 19:30:26 -0500

    Add getNumberOfQuestions function

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -35,4 +35,8 @@ class QuizBrain {
   String getQuestionText(int questionNumber) {
     return _questions[questionNumber].questionText;
   }
+
+  int getNumberOfQuestions() {
+    return _questions.length;
+  }
 }
