96fc4a873e304978f358d3c7fdc7ffd9deae994b
AndresLince <lynxdarmatron@gmail.com>
2023-07-02 10:44:07 -0500

    Remove duplicated questions

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -8,9 +8,6 @@ class QuizBrain {
   List<Widget> _scoreKeeper = [];
 
   List<Question> _questions = [
-    Question('You can lead a cow down stairs but not up stairs.', false),
-    Question('Approximately one quarter of human bones are in the feet.', true),
-    Question('A slug\'s blood is green.', true),
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
